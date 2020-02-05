95f01d4b1bda687742e3ef1c90beee19bd427d2a
dyzalonius <dyzalonius@gmail.com>
2020-02-05 15:47:27 +0100

    Add first step for BSP

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,69 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using Utils;
 
-public enum TileType {
-    Empty = 0,
-    Player,
-    Enemy,
-    Wall,
-    Door,
-    Key,
-    Dagger,
-    End
-}
-
 public class LevelGenerator : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField]
+    private Vector2Int gridSize;
+
+    [SerializeField]
+    [Range(0, 20)]
+    private int splitCount;
+
+    [SerializeField]
+    [Range(0f, 0.5f)]
+    private float minSplitPercentage;
+
+    [HideInInspector]
+    public static LevelGenerator Instance;
+
+    [HideInInspector]
     public GameObject[] tiles;
 
-    protected void Start()
-    {        
-        int width = 128;
-        int height = 128;
-        TileType[,] grid = new TileType[height, width];
+    [HideInInspector]
+    public TileType[,] Grid;
 
-        FillBlock(grid, 0, 0, width, height, TileType.Wall);
-        FillBlock(grid, 26, 26, 12, 12, TileType.Empty);
-        FillBlock(grid, 32, 28, 1, 1, TileType.Player);
-        FillBlock(grid, 30, 30, 1, 1, TileType.Dagger);
-        FillBlock(grid, 34, 30, 1, 1, TileType.Key);
-        FillBlock(grid, 32, 32, 1, 1, TileType.Door);
-        FillBlock(grid, 32, 36, 1, 1, TileType.Enemy);
-        FillBlock(grid, 32, 34, 1, 1, TileType.End);
-
-        Debugger.instance.AddLabel(32, 26, "Room 1");
-
-        //use 2d array (i.e. for using cellular automata)
-        CreateTilesFromArray(grid);
+    private void Start()
+    {
+        Instance = this;
+        GenerateLevel();
     }
 
-    //fill part of array with tiles
-    private void FillBlock(TileType[,] grid, int x, int y, int width, int height, TileType fillType) {
-        for (int tileY=0; tileY<height; tileY++) {
-            for (int tileX=0; tileX<width; tileX++) {
-                SetTile(grid, x + tileX, y + tileY, fillType);
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        ResetLevel();
+
+        Grid = new TileType[gridSize.x, gridSize.y];
+        SetBlock(0, 0, gridSize.x, gridSize.y, TileType.Wall);
+
+        Node root = new Node(0, 0, gridSize.x, gridSize.y);
+        root.Split(splitCount, minSplitPercentage);
+        root.Draw();
+
+        /*SetBlock(26, 26, 12, 12, TileType.Empty);
+        SetBlock(30, 30, 1, 1, TileType.Dagger);
+        SetBlock(34, 30, 1, 1, TileType.Key);
+        SetBlock(32, 32, 1, 1, TileType.Door);
+        SetBlock(32, 36, 1, 1, TileType.Enemy);
+        SetBlock(32, 34, 1, 1, TileType.End);*/
+        SetBlock(32, 28, 1, 1, TileType.Player);
+        //Debugger.instance.AddLabel(32, 26, "Room 1");
+
+        CreateLevel();
+    }
+
+    public void SetBlock(int x, int y, int width, int height, TileType fillType)
+    {
+        for (int tileY = 0; tileY < height; tileY++)
+        {
+            for (int tileX = 0; tileX < width; tileX++)
+            {
+                SetTile(x + tileX, y + tileY, fillType);
             }
         }
     }
 
-    private void SetTile(TileType[,] grid, int x, int y, TileType fillType)
+    private void SetTile(int x, int y, TileType fillType)
     {
         // Check bounds
         if (x < 0) return;
-        if (y < 9) return;
-        if (x >= grid.GetLength(0)) return;
-        if (y >= grid.GetLength(1)) return;
+        if (y < 0) return;
+        if (x >= Grid.GetLength(1)) return;
+        if (y >= Grid.GetLength(0)) return;
 
-        grid[x, y] = fillType;
+        Grid[y, x] = fillType;
     }
 
-    //use array to create tiles
-    private void CreateTilesFromArray(TileType[,] grid) {
-        int height = grid.GetLength(0);
-        int width = grid.GetLength(1);
+    private void CreateLevel() {
+        int height = Grid.GetLength(0);
+        int width = Grid.GetLength(1);
         for (int y=0; y<height; y++) {
             for (int x=0; x<width; x++) {
-                 TileType tile = grid[y, x];
+                 TileType tile = Grid[y, x];
                  if (tile != TileType.Empty) {
                      CreateTile(x, y, tile);
                  }
@@ -73,14 +96,13 @@ public class LevelGenerator : MonoBehaviour
         }
     }
 
-    //create a single tile
     private GameObject CreateTile(int x, int y, TileType type) {
         int tileID = ((int)type) - 1;
         if (tileID >= 0 && tileID < tiles.Length)
         {
             GameObject tilePrefab = tiles[tileID];
             if (tilePrefab != null) {
-                GameObject newTile = GameObject.Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                GameObject newTile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
                 newTile.transform.SetParent(transform);
                 return newTile;
             }
@@ -92,4 +114,24 @@ public class LevelGenerator : MonoBehaviour
         return null;
     }
 
+    private void ResetLevel()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+            Destroy(transform.GetChild(i).gameObject);
+
+        Debugger.instance.labels.ForEach(x => Destroy(x));
+        Debugger.instance.labels.Clear();
+    }
+}
+
+public enum TileType
+{
+    Empty = 0,
+    Player,
+    Enemy,
+    Wall,
+    Door,
+    Key,
+    Dagger,
+    End
 }
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class Node
+{
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Node[] children = new Node[2];
+    private bool hasChildren = false;
+    
+    public Node(int x, int y, int width, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void Split(int splitCount, float minSplitPercentage = 0.5f)
+    {
+        // Exit if no more splits left
+        if (splitCount <= 0)
+            return;
+        else
+            splitCount--;
+
+        // Calculate splitpercentage and if we are splitting through X axis or Y axis
+        float splitPercentage = Random.Range(minSplitPercentage, 1 - minSplitPercentage);
+        bool splitThroughX = width == height ? Random.value > 0.5 : width > height;
+
+        // Create new children
+        hasChildren = true;
+        if (splitThroughX)
+        {
+            Node child1 = new Node(x, y, Mathf.RoundToInt(width * splitPercentage), height);
+            Node child2 = new Node(x + Mathf.RoundToInt(width * splitPercentage), y, Mathf.RoundToInt(width * (1f - splitPercentage)), height);
+            children[0] = child1;
+            children[1] = child2;
+        }
+        else
+        {
+            Node child1 = new Node(x, y, width, Mathf.RoundToInt(height * splitPercentage));
+            Node child2 = new Node(x, y + Mathf.RoundToInt(height * splitPercentage), width, Mathf.RoundToInt(height * (1f - splitPercentage)));
+            children[0] = child1;
+            children[1] = child2;
+        }
+
+        // Split recursively for children
+        children[0].Split(splitCount, minSplitPercentage);
+        children[1].Split(splitCount, minSplitPercentage);
+    }
+
+    public void Draw()
+    {
+        if (hasChildren)
+        {
+            children[0].Draw();
+            children[1].Draw();
+        }
+        else
+        {
+            LevelGenerator.Instance.SetBlock(x, y, width, height, TileType.Wall);
+            LevelGenerator.Instance.SetBlock(x + 1, y + 1, width - 2, height - 2, TileType.Empty);
+            Debugger.instance.AddLabel(x + width/2, y + height/2, "ThisIsANode");
+        }
+    }
+}

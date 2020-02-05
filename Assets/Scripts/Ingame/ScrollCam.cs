555e125125991b4e15ea3384b488a8143e245448
dyzalonius <dyzalonius@gmail.com>
2020-02-05 13:18:01 +0100

    Initial project

---
+++ Assets/Scripts/Ingame/ScrollCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCam : MonoBehaviour
+{
+    [SerializeField]
+    private float minZoom;
+
+    [SerializeField]
+    private float maxZoom;
+
+    [SerializeField]
+    [Range(1, 5)]
+    private float zoomStep;
+
+    private void Update()
+    {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        float size = Camera.main.orthographicSize;
+
+        if (scroll < 0)
+            size *= zoomStep;
+
+        if (scroll > 0)
+            size /= zoomStep;
+
+        size = Mathf.Clamp(size, minZoom, maxZoom);
+
+        Camera.main.orthographicSize = size;
+    }
+}
